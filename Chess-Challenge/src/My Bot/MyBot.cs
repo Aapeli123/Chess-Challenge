cd092293c2de5eec4adb83143608b144dd00a09d
Aapo <aaposharju@gmail.com>
2023-07-22 15:00:49 +0300

    initial commit to fork

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,16 @@
 ﻿using ChessChallenge.API;
-
+using System.Collections.Generic;
 public class MyBot : IChessBot
 {
+    Dictionary<string, Move> lookup;
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        foreach (Move m in moves) {
+            board.MakeMove(m);
+            board.GetFenString();
+            board.UndoMove(m);
+        }
         return moves[0];
     }
 }
